05a72f0d3a422bc7f8798a2f8cb9c172cd17e355
Edson Silva <dinhovdp@gmail.com>
2026-06-12 10:46:39 -0300

    atividades e exercicios JAVA 6.3

---
+++ aula_02_variaveis/src/aula_02_variaveis/OperadoresUnarios.java
@@ -0,0 +1,18 @@
+package aula_02_variaveis;
+
+public class OperadoresUnarios {
+
+		public static void main(String[] args) {
+		
+			int contador = 1000;
+			
+			System.out.println("Valor inicial de contador: " + contador);
+			
+			//System.out.println("Valor Pré Inrementado: " + (++ contador));
+			
+			System.out.println("Valor Pós Inrementado: " + (contador ++));
+			
+			System.out.println("Novo valor de contador: " + + contador);
+		}
+}
+
