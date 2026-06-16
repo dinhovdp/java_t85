19efef225dde7d8194781b6adf5a6509a5c52ed8
Edson Silva <dinhovdp@gmail.com>
2026-06-16 09:01:02 -0300

    subindo exercicios 3

--- aula_03_condicionais/src/aula_03_condicionais/Exercicio_03.java
+++ aula_03_condicionais/src/aula_03_condicionais/Exercicio_03.java
@@ -12,7 +12,7 @@ public class Exercicio_03 {
 	
 	
 		System.out.println("Digite o seu nome: ");
-		nome = leia.next();
+		nome = leia.nextLine();
 		
 		System.out.println("Olá, " + nome + "\nSeja bem vindo ao nosso sistema de colaboradores!\n"
 				+ "Selecione a opção para descobrir qual foi o percentual de Reajuste de Salario"
@@ -23,7 +23,7 @@ public class Exercicio_03 {
 		System.out.println("********************************************");
 		System.out.println(" Por favor, escolha dentre as opções abaixo	");
 		System.out.println("										    ");
-		System.out.println("	1 - GERENTE			4 - MOTORISTA		");
+		System.out.println("	1 - GERENTE		4 - MOTORISTA			");
 		System.out.println("	2 - VENDEDOR	  	5 - ESTOQUISTA 	    ");
 		System.out.println("	3 - SUPERVISOR	 	6 - TECNICO DE TI   ");
 		System.out.println("											");
@@ -38,6 +38,13 @@ public class Exercicio_03 {
 					
 		System.out.println("Escolha a Opção Desejada: ");
 		opcao = leia.nextInt();
+		
+		if (opcao >= 1 || opcao <= 6) {
+			System.out.println("Você escolheu um cargo corretamente.\n");
+			
+		}else {
+				System.out.println("Você escolheu uma opção invalida");
+		}
 		
 		int num1 = 0;
 		System.out.println("Insira o valor do salário: ");
@@ -57,66 +64,59 @@ public class Exercicio_03 {
 		
 		case 1:
 			System.out.printf(" 1 - A opção selecionada é GERENTE.\n"
-					+ "O Reajuste salarial foi de: 10%");
+					+ "O Reajuste salarial foi de: 10%%");
 			
 			resultado = num1 + (num1 * gerente);
 			System.out.printf(" O seu novo salario é %.2f", resultado);
-			System.out.println("!!!Obrigado por utilizar nosso Sistema!!!");
+			System.out.println("\n!!!Obrigado por utilizar nosso Sistema!!!");
 			break;
 		case 2:
 			System.out.printf(" 1 - A opção selecionada é VENDEDOR.\n"
-					+ "O Reajuste salarial foi de: 7%");
+					+ "O Reajuste salarial foi de: 7%%");
 			
 			resultado = num1 + (num1 * vendedor);
 			System.out.printf(" O seu novo salario é %.2f", resultado);
-			System.out.println("!!!Obrigado por utilizar nosso Sistema!!!");
+			System.out.println("\n!!!Obrigado por utilizar nosso Sistema!!!");
 			break;
 		case 3:
 			System.out.printf(" 1 - A opção selecionada é SUPERVISOR.\n"
-					+ "O Reajuste salarial foi de: 9%");
+					+ "O Reajuste salarial foi de: 9%%");
 			
 			resultado = num1 + (num1 * supervisor);
 			System.out.printf(" O seu novo salario é %.2f", resultado);
-			System.out.println("!!!Obrigado por utilizar nosso Sistema!!!");
+			System.out.println("\n!!!Obrigado por utilizar nosso Sistema!!!");
 			break;
 		case 4:
 			System.out.printf(" 1 - A opção selecionada é MOTORISTA.\n"
-					+ "O Reajuste salarial foi de: 6%");
+					+ "O Reajuste salarial foi de: 6%%");
 			
 			resultado = num1 + (num1 * motorista);
 			System.out.printf(" O seu novo salario é %.2f", resultado);
-			System.out.println("!!!Obrigado por utilizar nosso Sistema!!!");
+			System.out.println("\n!!!Obrigado por utilizar nosso Sistema!!!");
 			break;
 		case 5:
 			System.out.printf(" 1 - A opção selecionada é ESTOQUISTA.\n"
-					+ "O Reajuste salarial foi de: 5%");
+					+ "O Reajuste salarial foi de: 5%%");
 			
 			resultado = num1 + (num1 * estoquista);
 			System.out.printf(" O seu novo salario é %.2f", resultado);
-			System.out.println("!!!Obrigado por utilizar nosso Sistema!!!");
+			System.out.println("\n!!!Obrigado por utilizar nosso Sistema!!!");
 			break;
 		case 6:
 			System.out.printf(" 1 - A opção selecionada é TECNICO DE TI.\n"
-					+ "O Reajuste salarial foi de: 8%");
+					+ "O Reajuste salarial foi de: 8%%");
 			
 			resultado = num1 + (num1 * tecnicoTi);
 			System.out.printf(" O seu novo salario é %.2f", resultado);
-			System.out.println("!!!Obrigado por utilizar nosso Sistema!!!");
+			System.out.println("\n!!!Obrigado por utilizar nosso Sistema!!!");
 			break;
 			
 		default:
-			System.out.println("!!!Obrigado por utilizar nosso Sistema!!!");
+			System.out.println("\n!!!Obrigado por utilizar nosso Sistema!!!");
 			break;
 	}
-		}
-			
 		
-	
-		
-
-		
-		
-		
+					
 		
 		leia.close();
 	}
