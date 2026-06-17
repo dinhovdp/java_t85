6f40f511dee802c7f8ec8a815d8732ba8ee644a4
Edson Silva <dinhovdp@gmail.com>
2026-06-17 00:38:14 -0300

    atualizando novamente

--- aula_04_repeticoes/src/aula_04_repeticoes/Exercicios_Lista_02.java
+++ aula_04_repeticoes/src/aula_04_repeticoes/Exercicios_Lista_02.java
@@ -6,37 +6,51 @@ import java.util.Scanner;
 public class Exercicios_Lista_02 {
 
 	public static void main(String[] args) {
-		
-		
+				
 		
 		Scanner leia = new Scanner(System.in);
-
+		String caractere;
 		
-		int idade = 0, menor21 = 0, maior50 = 0;
+
+		System.out.println("Insira um numero negativo para encerrar o Programa\n");
+		
+		int idade = 0, mediaIdade = 0, menor21 = 0, maior50 = 0;
 		
 		while(idade>=0) {
+			
 		System.out.print("Digite sua idade: ");
 		idade = leia.nextInt();
 		 
 		
 		if (idade<0)
-			System.out.println("Leitura de dados finalizada.");
+			System.out.println("Contagem de  finalizada.");
 		
 		
 		//Contagem de pessoas com menos de 21 anos.
 		if (idade < 21 && idade >= 0)
 			menor21 ++;
-		System.out.printf("\nTotal de pessoas menores de 21 anos: %d", menor21);
+		
 		
 		//Contagem de pessoas com mais de 50 anos;
 		if (idade > 50)
 			maior50 ++;
-		System.out.printf("\nTotal de pessoas maiores de 50 anos: %d", maior50);
+		
+		//Contagem de pessoas com idade entre 21 e 50 anos;
+				if (idade <= 50  && idade >= 21)
+					mediaIdade ++;
+				
+				if (caractere)
+					System.out.println("Caractere Invalido!\nPrograma Finalizado.");
+				
+		
 		}
 		
+		System.out.printf("\nTotal de pessoas menores de 21 anos: %d", menor21);
+		System.out.printf("\nTotal de pessoas maiores de 50 anos: %d", maior50);
+		System.out.printf("\nTotal de pessoas entre 21 e 50 anos: %d", mediaIdade);
 		
-		
-		
+		int resultado = menor21 + maior50 + mediaIdade;
+		System.out.printf("\nTotal de pessoas que participaram da pesquisa %d", resultado);
 		
 		leia.close();
 	}
