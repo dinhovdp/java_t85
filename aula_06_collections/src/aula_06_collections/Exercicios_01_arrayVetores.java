77b13962edc0935d7ee5d737d6bcd076ef3460f2
Edson Silva <dinhovdp@gmail.com>
2026-06-17 22:35:22 -0300

    arrays vetores e matriz

---
+++ aula_06_collections/src/aula_06_collections/Exercicios_01_arrayVetores.java
@@ -0,0 +1,33 @@
+package aula_06_collections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Exercicios_01_arrayVetores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+						 
+		ArrayList <String> coresType = new ArrayList <String> ();
+	        
+		coresType.add("Amarelo");
+		coresType.add("Azul");
+		coresType.add("Branco");
+		coresType.add("Laranja");
+		coresType.add("Verde");
+		
+		
+				
+	        for(String cor: coresType) {
+				System.out.println(cor);
+				
+				
+	        }
+	        
+	        
+		
+	}
+
+}
